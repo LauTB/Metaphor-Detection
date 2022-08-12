30559fb7ef2cd41782e6287e90e3155f3b3faedf
LauTB <45464137+LauTB@users.noreply.github.com>
2022-08-12 16:22:12 -0400

    completed scrapper

--- vanguardia/vanguardia/spiders/vanspider.py
+++ vanguardia/vanguardia/spiders/vanspider.py
@@ -2,31 +2,61 @@ import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 from vanguardia.items import VanguardiaItem
-
-
+import os
 class VanspiderSpider(CrawlSpider):
     name = 'vanspider'
-    allowed_domains = ['www.vanguardia.cu']
-    start_urls = ['http://www.vanguardia.cu']
-    rules = (
-    Rule(LinkExtractor(allow = r'/ediciones-impresas'),#
-         callback = 'parse_item',
-         follow = True),
-)
+    def __init__(self):
+        super(VanspiderSpider,self).__init__()
+        self.allowed_domains = ['www.vanguardia.cu']
+        self.start_urls = ['http://www.vanguardia.cu/ediciones-impresas']
+        self.rules = (
+        Rule(LinkExtractor(allow = r'/ediciones-impresas'),#
+            callback = 'parse',
+            follow = True),)
 
-    def parse_item(self, response):
-        page_number = 1
-        file_local_url = response.css(f'ul.list-group:nth-child(1) > li:nth-child({page_number}) > span:nth-child(1) > a:nth-child(1)::attr(href)').get()
-        while file_local_url is not None:
-            file_url = response.urljoin(file_local_url)
-            item = VanguardiaItem()
-            item['file_urls'] = [file_url]
-            yield item
-            page_number += 1
-            file_local_url = response.css(f'ul.list-group:nth-child(1) > li:nth-child({page_number}) > span:nth-child(1) > a:nth-child(1)::attr(href)').get()
+    def _get_option_value(self, selector):
+        return selector.split(">")[0].split('value')[1][2:-1]
+
+    def parse(self, response):
+        year_index = 1
+        year_selector = response.css(f'#jform_editionyear > option:nth-child({year_index})').get()
+        while year_selector:
+            year_index +=1
+            year = self._get_option_value(year_selector)
+            month_index = 1
+            month_selector = response.css(f'#jform_editionmonth > option:nth-child({month_index})').get()
+            while month_selector:
+                month_index += 1
+                month = self._get_option_value(month_selector)
+                month_selector = response.css(f'#jform_editionmonth > option:nth-child({month_index})').get()
+                edition_index = 1
+                edition_selector = response.css(f'#jform_number > option:nth-child({edition_index})').get()
+                while edition_selector:
+                    edition_index += 1
+                    edition = self._get_option_value(edition_selector)                    
+                    data = {'jform[editionyear]':year, 
+                            'jform[editionmonth]':month,
+                            'jform[number]': edition,
+                            'jform[catid]':'123',
+                            'edecbec821f00c12637194a218cd5a6d':'1'}
+                    yield scrapy.FormRequest.from_response(response, url=f'http://www.vanguardia.cu/ediciones-impresas?task=edition.search',formcss = '#edition-form', formdata= data,callback= self.parse_edition)
+                    edition_selector = response.css(f'#jform_number > option:nth-child({edition_index})').get()
+            year_selector = response.css(f'#jform_editionyear > option:nth-child({year_index})').get()
+    
+    def parse_edition(self, response):
+        empty = response.css('body > div > div > main > section > div > h1').get()
+        not_found = 'no encontrada'
+        if empty is not None and not_found in empty:
+            path = os.path.join(os.getcwd(), "missing.txt")
+            with open(path, 'a') as file:
+                file.write(response.url+'\n')
         else:
-            print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
-            print(response.url)
-            month,edition =  '04','04'
-            self.year-=1
-            yield scrapy.FormRequest.from_response(response,url = f'http://www.vanguardia.cu/ediciones-impresas?year={year}&month={month}&number={edition}&category=123',  callback=self.parse_item)#formname='adminForm',formdata={'editionyear': '2022', 'editionmonth':'07', 'number': '03','catid':'123'},
+            page_number = 1
+            file_local_url = response.css(f'ul.list-group:nth-child(1) > li:nth-child({page_number}) > span:nth-child(1) > a:nth-child(1)::attr(href)').get()
+            while file_local_url is not None:
+                file_url = response.urljoin(file_local_url)
+                item = VanguardiaItem()
+                item['file_urls'] = [file_url]
+                yield item
+                page_number += 1
+                file_local_url = response.css(f'ul.list-group:nth-child(1) > li:nth-child({page_number}) > span:nth-child(1) > a:nth-child(1)::attr(href)').get()
