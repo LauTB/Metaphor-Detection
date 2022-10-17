769cbf63033d525b027968d7537799c20de64a14
LauTB <45464137+LauTB@users.noreply.github.com>
2022-10-17 14:53:10 -0400

    process pdfs files done

--- corpus/process_files.py
+++ corpus/process_files.py
@@ -1,115 +1,56 @@
-import json
 import os
-import PyPDF2 
-from nltk.tokenize import sent_tokenize, word_tokenize 
-from get_files import get_date_from_file
-import spacy
 import csv
-#nlp = spacy.load("es_core_news_sm")
-def get_text(pdf_path):   
-    # creating a pdf file object 
-    pdfFileObj = open(pdf_path, 'rb') 
-        
-    # creating a pdf reader object 
-    pdfReader = PyPDF2.PdfFileReader(pdfFileObj) 
-    total_text = []
-    # printing number of pages in pdf file 
-    num = pdfReader.numPages 
-    for i in range(num):
-        # creating a page object 
-        pageObj = pdfReader.getPage(i)
-        # extracting text from page 
-        total_text.append(pageObj.extractText())         
-    # closing the pdf file object 
-    pdfFileObj.close() 
-    return total_text
+import spacy
+from PyPDF2 import PdfFileReader, PdfFileWriter
+from itertools import chain
+from get_files import get_date_from_file
 
-def get_all_editions(dir = r'corpus\downloads'):
+nlp = spacy.load("es_core_news_sm")
+
+def join_editions_path(dir = r'corpus\downloads'):
     all = os.listdir(dir)
     editions = {}
     for p in all:
         if p.endswith('.pdf'):
             path = os.path.join(dir, p)
-            text = get_text(path)
             date = get_date_from_file(p)
             try:
-                editions[date].append(text)
+                editions[date].append(path)
             except KeyError:
-                editions[date] =[ text]
+                editions[date] =[path]
     return editions
 
-def get_sentences(data):
-    
-    sen = sent_tokenize(data[0])
-    fixed_sentences = []
-    for t in sen:
-        words = word_tokenize(t)
-        fixed_words = []
-        for w in words:
-            t = w.split('-')
-            if len(t) > 1:
-                t = t[0]+t[1]
-            else:
-                t = t[0]
-            fixed_words.append(t)
-        fixed_sentences.append(' '. join(f for f in fixed_words))
-    return fixed_sentences
+def merge_pdfs(paths, output):
+    pdf_writer = PdfFileWriter()
 
-def write_first_json():
-    my_dict = get_all_editions()
-    with open("data_file.json", "w", encoding = 'utf-8') as write_file:
-        json.dump(my_dict, write_file, ensure_ascii=False)
+    for path in paths:
+        pdf_reader = PdfFileReader(path)
+        for page in range(pdf_reader.getNumPages()):
+            # Add each page to the writer object
+            pdf_writer.addPage(pdf_reader.getPage(page))
+    # Write out the merged PDF
+    with open(output, 'wb') as out:
+        pdf_writer.write(out)
 
+def create_pdfs():
+    editions_paths = join_editions_path()
+    for date in editions_paths:
+        path = os.path.join(r'corpus\pdfs', date+'.pdf')
+        merge_pdfs(editions_paths[date],path)
 
-def write_sentence_json():
-    with open("data_file.json", "r", encoding = 'utf-8') as read_it:
-        data = json.load(read_it)
-        sentence = []
-        for d in data.keys():
-            all_text = data[d]
-            for text in all_text:
-                all_sents = get_sentences(text)
-                for sen in all_sents:
-                    sentence.append((d, sen))
-        with open("sentence_file.json", "w", encoding = 'utf-8') as write_file:
-            json.dump(sentence, write_file, ensure_ascii=False)
-def create_csv(nlp):
-    with open("sentence_file.json", "r", encoding = 'utf-8') as read_it:
-        data = json.load(read_it)
-        new_data = []
-        for date, text in data:
-            processed_text = nlp(text)
-            tags = []
-            for token in processed_text:
-                tags.append(token.pos_)
-            with open(f'corpus\\csv\\{date}.csv', mode='a', encoding='utf-8') as file:
-                writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-                writer.writerow([date, text, ' '.join(t for t in tags)])
-
-path = r'corpus\csv\2014-05-04.csv'
-# with open(path, 'r', encoding = 'utf-8') as csv_file :
-#     csv_reader = csv.reader(csv_file, delimiter=',')
-#     line = 0
-#     for row in csv_reader:
-#         line+=1
-#         if line % 2 == 0:
-#             continue
-#         p = row[1]
-#         text = nlp(p)
-#         print(p)
-#         noun_adj_pairs = {}
-#         for chunk in text.noun_chunks:
-#             adj = []
-#             noun = ""
-#             print(chunk)
-#             for tok in chunk:
-#                 if tok.pos_ == "NOUN":
-#                     noun = tok.text
-#                 if tok.pos_ == "ADJ":
-#                     adj.append(tok.text)
-#             if noun:
-#                 noun_adj_pairs.update({noun:adj})
-#         print(noun_adj_pairs)
-#         input()
-get_all_editions()
-
+def extract_an_from_pdf(path,save_path):
+    with open(path, 'r', encoding = 'utf-8') as csv_file :
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        an_set = set()
+        for row in csv_reader:
+            text = row[0]
+            doc = nlp(text)
+            for token in doc:
+                if token.pos_ == 'ADJ':
+                    for related in chain(token.lefts, token.rights):
+                        if related.pos_ == 'NOUN':
+                            pair = (str.lower(token.text), str.lower(related.text))
+                            an_set.add(pair)
+        with open(save_path, 'w+', encoding = 'utf-8', newline='') as csv_save_file :
+            csv_writer = csv.writer(csv_save_file)
+            csv_writer.writerows(list(an_set))
